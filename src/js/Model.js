713468caab3a4f319bbc2bcb0b665d8ecd702142
wangfan <i.wangfan@foxmail.com>
2018-03-26 23:11:49 +0800

    update

--- src/js/Model.js
+++ src/js/Model.js
@@ -2,6 +2,29 @@ import EventEmit from '../lib/EventEmitter.js'
 import '../../node_modules/leancloud-storage/dist/av-min.js'
 
 var Model = function() {
+    const appId = 'pDAVsDD9SILklHldH52mAXoN-gzGzoHsz'
+    const appKey = 'R8grFdtFaI0vg3si8rubvmKL'
+    AV.init({ appId, appKey })
+    localStorage.setItem('debug', 'leancloud*');
+
+    const Todo = AV.Object.extend('Todo');
+    let todo = new Todo()
+    todo.set('title', "测试数据一")
+    todo.save()
+    .then(function(data) {
+        console.log(data)
+    }).catch(function(e) {
+
+    })
+
+    var query = new AV.Query('Todo')
+    query.find()
+    .then(function(data) {
+        console.log(data)
+    })   
+    .catch(function(e) {
+
+    })
     console.log(AV)
     var lists = ['代办1','代办2','代办3','代办4','代办5',]
     var emit = Object.create(EventEmit.prototype)
