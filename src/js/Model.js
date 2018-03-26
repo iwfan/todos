78d95713493064c2fb9aa642f262795988b7c88e
wangfan <i.wangfan@foxmail.com>
2018-03-26 23:30:26 +0800

    update

--- src/js/Model.js
+++ src/js/Model.js
@@ -2,30 +2,47 @@ import EventEmit from '../lib/EventEmitter.js'
 import '../../node_modules/leancloud-storage/dist/av-min.js'
 
 var Model = function() {
+    // init
     const appId = 'pDAVsDD9SILklHldH52mAXoN-gzGzoHsz'
     const appKey = 'R8grFdtFaI0vg3si8rubvmKL'
     AV.init({ appId, appKey })
     localStorage.setItem('debug', 'leancloud*');
 
+    // create class
     const Todo = AV.Object.extend('Todo');
-    let todo = new Todo()
-    todo.set('title', "测试数据一")
-    todo.save()
-    .then(function(data) {
-        console.log(data)
-    }).catch(function(e) {
+    const todoList
 
-    })
+    // let todo = new Todo()
+    // todo.set('title', "测试数据一")
+    // todo.save()
+    // .then(function(data) {
+    //     console.log(data)
+    // }).catch(function(e) {
 
-    var query = new AV.Query('Todo')
-    query.find()
-    .then(function(data) {
-        console.log(data)
-    })   
-    .catch(function(e) {
+    // })
 
-    })
-    console.log(AV)
+    // var query = new AV.Query('Todo')
+    // query.find()
+    // .then(function(data) {
+    //     console.log(data)
+    // })   
+    // .catch(function(e) {
+
+    // })
+    // console.log(AV)
+
+    let lists 
+
+    try {
+       lists = await fetchTodoData() 
+    } catch (error) {
+       lists = [] 
+    }
+
+    async function fetchTodoData() {
+        return await new AV.Query('Todo').find()
+    }
+
     var lists = ['代办1','代办2','代办3','代办4','代办5',]
     var emit = Object.create(EventEmit.prototype)
     var handler = {
@@ -41,7 +58,7 @@ var Model = function() {
             return result
         }
     }
-    var api = {
+    const API = {
         add: new Proxy(function add(data) {
             return lists.push(data)
         }, handler),
@@ -52,7 +69,7 @@ var Model = function() {
             return lists;
         }
     }
-    return Object.assign(emit, api)
+    return Object.assign(emit, API)
 }
 
 export default Model
