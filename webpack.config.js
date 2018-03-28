16be32f93a1d9d7ff18e25b1cbc692c9a80c3f67
wangfan <i.wangfan@foxmail.com>
2018-03-28 18:23:01 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path')
 
+const env = process.env.NODE_ENV
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
+    },
+    config: {
+        mode: env || 'development',
     },
 }
